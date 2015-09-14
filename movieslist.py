7b1fd4fb4d6adaee5977b49d498d5cd05a44e61b
nemanjas1990 <nemanjas1990@live.com>
2015-09-14 21:53:44 +0200

    Adding Files...

---
+++ movieslist.py
@@ -0,0 +1,76 @@
+from basicmovie import Movie
+import basicmovie
+import webbrowser
+
+class ForrestGump(Movie):
+    def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+    
+class Inception(Movie):
+    def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+
+class OneFlewOvertheCuckoosNest(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+
+class Shichininnosamurai(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+class Avatar(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+class ShawshankRedemption(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+
+class Godfather(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+
+class DarkKnight(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+
+class SchindlersList(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+                 
+class PulpFiction(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+                 
+class Ilbuonoilbruttoilcattivo(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+                 
+class ReturnoftheKing(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+                 
+class FightClub(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+                 
+class FellowshipoftheRing(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
+                 
+class EmpireStrikesBack(Movie):
+     def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url, movie_producer):
+                 Movie.__init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url)
+                 self.movie_producer = movie_producer
