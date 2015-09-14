9ee41756d4ec24dd3bfd9fe293a62e80ce4aeb5f
nemanjas1990 <nemanjas1990@live.com>
2015-09-14 21:52:25 +0200

    Revert "Adding Files..."
    This reverts commit f83a56050cd571016696e84c1515bcc249f12810.

--- basicmovie.py
+++
@@ -1,8 +0,0 @@
-class Movie():
-    def __init__(self, movie_title, movie_story, poster_image_url, trailer_youtube_url):
-        self.title = movie_title
-        self.story = movie_story
-        self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
-
-   
